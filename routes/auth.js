a5d85fe4fd163709590a9f848252dfab1ea934be
Jawad Jahangir <jawad.jahangir.666@gmail.com>
2019-03-29 23:31:15 +0600

    default get route removed

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,10 @@
 const Authentication = require("../controllers/authentication");
 const passportServices = require("../services/passport");
 const passport = require("passport");
-const requireAuth = passport.authenticate("jwt", { session: false });
+//const requireAuth = passport.authenticate("jwt", { session: false });
 //const requireSignin = passport.authenticate("local", { session: false });
 
 module.exports = function(app) {
-  app.get("/", requireAuth, function(req, res) {
-    res.send({ hi: "there" });
-  });
   app.post("/api/users/signup", Authentication.signup);
   app.post("/api/users/signin", Authentication.signin);
 };
